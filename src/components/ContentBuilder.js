3e054d761ef6661488099f54f0a7293524bd56e9
willglass11 <billglass85.1150@gmail.com>
2022-07-18 15:49:09 -0500

    completely redid navbar, css, and now mobile friendly

--- src/components/ContentBuilder.js
+++ src/components/ContentBuilder.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import NavBar from './NavBar'
+import NavBar from './NavBar/NavBar'
 import ProductCards from './ProductCards'
 import Grid from '@mui/material/Grid';
 
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,106 @@
+import React, { useState } from 'react';
+import Dropdown from './Dropdown';
+
+import '../../styles/Navbar.css';
+import '../../styles/SignUpBtn.css';
+
+const isSignUp = true;
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const [dropdown, setDropdown] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const onMouseEnter = () => {
+    if (window.innerWidth < 960) {
+      setDropdown(false);
+    } else {
+      setDropdown(true);
+    }
+  };
+
+  const onMouseLeave = () => {
+    if (window.innerWidth < 960) {
+      setDropdown(false);
+    } else {
+      setDropdown(false);
+    }
+  };
+
+  return (
+    <>
+      <nav className='navbar'>
+        <div to='/' className='navbar-logo' onClick={closeMobileMenu}>
+          eCommerce
+        </div>
+        <div className='menu-icon' onClick={handleClick}>
+          <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+        </div>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+          <li className='nav-item'>
+            <div 
+            className='nav-links' 
+            onClick={closeMobileMenu}
+            >
+              Home
+            </div>
+          </li>
+          <li
+            className='nav-item'
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+          >
+            <div
+              className='nav-links'
+              onClick={closeMobileMenu}
+            >
+              Services <i className='fas fa-caret-down' />
+            </div>
+            {dropdown && <Dropdown />}
+          </li>
+          <li className='nav-item'>
+            <div
+              className='nav-links'
+              onClick={closeMobileMenu}
+            >
+              Products
+            </div>
+          </li>
+          <li className='nav-item'>
+            <div
+              className='nav-links'
+              onClick={closeMobileMenu}
+            >
+              Contact Us
+            </div>
+          </li>
+          <li>
+            <div
+              className='nav-links-mobile'
+              onClick={closeMobileMenu}
+            >
+              Sign Up
+            </div>
+          </li>
+        </ul>
+        {isSignUp ? 
+          <li className='nav-item'>
+            <div className={'btn'}>
+              Sign Up
+            </div>
+          </li> : 
+          <li className='nav-item'>
+            <div
+            className={'nav-linkss'}
+            >
+              Account
+            </div>
+          </li>}
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
