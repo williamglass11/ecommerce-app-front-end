f2dcceddf0cfe36f828d278f0ebbe16ab6c35531
willglass11 <billglass85.1150@gmail.com>
2022-07-27 15:37:33 -0500

    added footer, lots of content within, other css

--- src/components/ContentBuilder.js
+++ src/components/ContentBuilder.js
@@ -1,59 +1,81 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import NavBar from './NavBar/NavBar';
-import ProductCards from './pages/ProductCards';
-import Grid from '@mui/material/Grid';
-import HomePage from './pages/HomePage';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./NavBar/NavBar";
+import ProductCards from "./pages/ProductCards";
+import Grid from "@mui/material/Grid";
+import Footer from "./Footer";
+import HomePage from "./pages/HomePage";
 
-import book from '../styles/BatteredBookImage.webp'
-import shoes from '../styles/03-crocs-by-carrots-gq-september-2020.webp'
-import hat from '../styles/hat.jpg'
-import controller from '../styles/61sQsA4NpXL._AC_SX679_.jpg'
-import headset from '../styles/headset.webp'
-import logo from '../styles/logo192.png'
-import logo1 from '../styles/react-icon-dark.jpg'
+import book from "../styles/BatteredBookImage.webp";
+import shoes from "../styles/03-crocs-by-carrots-gq-september-2020.webp";
+import hat from "../styles/hat.jpg";
+import controller from "../styles/61sQsA4NpXL._AC_SX679_.jpg";
+import headset from "../styles/headset.webp";
+import logo from "../styles/logo192.png";
+import logo1 from "../styles/react-icon-dark.jpg";
 
-import '../styles/App.css'
+import "../styles/App.css";
 
 const products = [
-  {id: 1, name: 'book', description: 'a wonderful book to read on your own time', image: book},
-  {id: 2, name: 'shoes', description: 'nike running shoes', image: shoes},
-  {id: 3, name: 'hat', description: 'you can wear this thing on your head its insane', image: hat },
-  {id: 4, name: 'controller', description: 'used to control input into a computing device, such as a game', image: controller},
-  {id: 5, name: 'headset', description: 'can be used to hear audio output from a device', image: headset},
-  {id: 6, name: 'bread', description: 'tasty', image: logo1},
-  {id: 7, name: 'toilet', description: 'you know what it is', image: logo},
-  {id: 6, name: 'bread', description: 'tasty', image: logo1},
-  {id: 7, name: 'toilet', description: 'you know what it is', image: logo},
-  {id: 6, name: 'bread', description: 'tasty', image: logo1},
-  {id: 7, name: 'toilet', description: 'you know what it is', image: logo},
-  {id: 6, name: 'bread', description: 'tasty', image: logo1},
-  {id: 7, name: 'toilet', description: 'you know what it is', image: logo}
-]
+  {
+    id: 1,
+    name: "book",
+    description: "a wonderful book to read on your own time",
+    image: book,
+  },
+  { id: 2, name: "shoes", description: "nike running shoes", image: shoes },
+  {
+    id: 3,
+    name: "hat",
+    description: "you can wear this thing on your head its insane",
+    image: hat,
+  },
+  {
+    id: 4,
+    name: "controller",
+    description:
+      "used to control input into a computing device, such as a game",
+    image: controller,
+  },
+  {
+    id: 5,
+    name: "headset",
+    description: "can be used to hear audio output from a device",
+    image: headset,
+  },
+  { id: 6, name: "bread", description: "tasty", image: logo1 },
+  { id: 7, name: "toilet", description: "you know what it is", image: logo },
+  { id: 6, name: "bread", description: "tasty", image: logo1 },
+  { id: 7, name: "toilet", description: "you know what it is", image: logo },
+  { id: 6, name: "bread", description: "tasty", image: logo1 },
+  { id: 7, name: "toilet", description: "you know what it is", image: logo },
+  { id: 6, name: "bread", description: "tasty", image: logo1 },
+  { id: 7, name: "toilet", description: "you know what it is", image: logo },
+];
 
 const ContentBuilder = ({ pageRequest }) => {
-
-  console.log(pageRequest)
+  console.log(pageRequest);
   return (
     <Router>
-      <div className='mainPageContainer' data-testid='mainPageContainer'>
-      <NavBar />
-      <Routes>
-        <Route path='/' exact element={<HomePage />} />
-        <Route path='/products' element={
-          <div className='cardContainer'>
-            <Grid container justify='center' spacing={2.5} style={{ margin: 2 }} className='gridContainer'>
-              <ProductCards
-                products={products}
-              />
-            </Grid>
-          </div>}/>
-      </Routes>
-      <footer>
-      </footer>
+      <div className="mainPageContainer" data-testid="mainPageContainer">
+        <NavBar />
+        <Routes>
+          <Route path="/" exact element={<HomePage />} />
+          <Route
+            path="/products"
+            element={
+              <div className="cardContainer">
+                <Grid container className="gridContainer">
+                  <ProductCards products={products} />
+                </Grid>
+              </div>
+            }
+          />
+        </Routes>
+        <Footer />
       </div>
     </Router>
-  )
-}
+  );
+};
 
-export default ContentBuilder
+export default ContentBuilder;
--- src/components/pages/ProductCards.js
+++ src/components/pages/ProductCards.js
@@ -1,58 +1,62 @@
+import * as React from "react";
+import Grid from "@mui/material/Grid";
+import { makeStyles } from "@mui/styles";
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import { AddShoppingCart } from "@mui/icons-material";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 
-import * as React from 'react';
-import Grid from '@mui/material/Grid';
-import { makeStyles } from '@mui/styles';
-import { Card, CardMedia, CardContent, CardActions, IconButton, Typography } from '@mui/material';
-import { AddShoppingCart } from '@mui/icons-material';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-
-import '../../styles/App.css'
+import "../../styles/App.css";
 
 const useStyles = makeStyles({
   root: {
     width: 290,
-    height: 300
+    height: 300,
+    margin: 10,
   },
   media: {
-    height: 140
-  }
-})
+    height: 140,
+  },
+});
 
 export default function ProductCards(products) {
   const classes = useStyles();
   return (
     <>
-    {products.products.map((product, index) => (
-    <Grid item key={index}>
-      <Card className={classes.root}>
-      <CardMedia
-        className={classes.media}
-        component="img"
-        height="140"
-        image={product.image}
-        alt={product.name}
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {product.name}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {product.description}
-        </Typography>
-      </CardContent>
-        <CardActions disableSpacing>
-          <IconButton aria-label="add to favorites">
-            <FavoriteIcon />
-          </IconButton>
-          <IconButton aria-label="add to cart">
-            <AddShoppingCart />
-          </IconButton>
-        </CardActions>
-      </Card>
-    </Grid>
-    ))}
+      {products.products.map((product, index) => (
+        <Grid item key={index}>
+          <Card className={classes.root}>
+            <CardMedia
+              className={classes.media}
+              component="img"
+              image={product.image}
+              alt={product.name}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {product.name}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {product.description}
+              </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+              <IconButton aria-label="add to favorites">
+                <FavoriteIcon />
+              </IconButton>
+              <IconButton aria-label="add to cart">
+                <AddShoppingCart />
+              </IconButton>
+            </CardActions>
+          </Card>
+        </Grid>
+      ))}
     </>
   );
-  
 }
-
